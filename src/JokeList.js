dd863ef21b26ed2390fd3fcb42b13c9d9f59272c
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-26 22:15:44 -0700

    finished logic to sort by votes jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -68,6 +68,7 @@ class JokeList extends Component {
                 </div>
             )
         }
+        let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes)
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
@@ -77,7 +78,7 @@ class JokeList extends Component {
                     <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 <div className="JokeList-jokes">
-                    {this.state.jokes.map(j => (
+                    {jokes.map(j => (
                         <Joke
                             key={j.id}
                             votes={j.votes}
